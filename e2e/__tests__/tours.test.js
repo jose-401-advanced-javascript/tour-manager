5a4d687cee7939161f7cdf8e8c53c488bb833354
Jose <j.a.ojeda@live.com>
2019-10-01 16:46:34 -0700

    tested tour post method

--- e2e/__tests__/tours.test.js
+++ e2e/__tests__/tours.test.js
@@ -2,7 +2,7 @@ const request = require('../request');
 const db = require('../db');
 const { matchMongoId } = require('../match-helpers');
 
-describe('locations api', () => {
+describe('Tour api', () => {
   beforeEach(() => {
     return db.dropCollection('tours');
   });
@@ -10,14 +10,15 @@ describe('locations api', () => {
   const tour = {
     title: 'Test Tour',
     activities: ['Coding', 'Testing'],
-    launchDate: {},
-    stops: [{
-      location: {
-        latitude: 45.5266975,
-        longitude: -122.6880503
-      },
-      attendance: 1
-    }],
+    stops: [
+      {
+        location: {
+          latitude: 45.5266975,
+          longitude: -122.6880503
+        },
+        attendance: 1
+      }
+    ]
   };
 
   function postTour(tour) {
@@ -28,4 +29,33 @@ describe('locations api', () => {
       .then(({ body }) => body);
   }
 
+  it('adds a tour', () => {
+    return postTour(tour).then(tour => {
+      expect(tour).toMatchInlineSnapshot(
+        matchMongoId,
+        `
+        Object {
+          "__v": 0,
+          "_id": StringMatching /\\^\\[a-f\\\\d\\]\\{24\\}\\$/i,
+          "activities": Array [
+            "Coding",
+            "Testing",
+          ],
+          "launchDate": "2019-10-01T23:24:24.795Z",
+          "stops": Array [
+            Object {
+              "_id": "5d93e0288eff0a44658b058b",
+              "attendance": 1,
+              "location": Object {
+                "latitude": "1970-01-01T00:00:00.045Z",
+                "longitude": "1969-12-31T23:59:59.878Z",
+              },
+            },
+          ],
+          "title": "Test Tour",
+        }
+      `
+      );
+    });
+  });
 });
